cb83d767c62fa56bdab0b73c17f9efa71bb1e11b
Kaito <carlosmontes002@gmail.com>
2020-10-18 16:47:13 -0300

    move to another folder

---
+++ precache-manifest.251f8bb50ef56b2abea02dfa729f283d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32f50699994c7120a9c05de997bb81eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4897bef65735aefa9de3",
+    "url": "/static/css/2.f669d1ef.chunk.css"
+  },
+  {
+    "revision": "4897bef65735aefa9de3",
+    "url": "/static/js/2.2a5dc074.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.2a5dc074.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86916922690ca616c572",
+    "url": "/static/js/main.12ec2b98.chunk.js"
+  },
+  {
+    "revision": "0af071cdb6470818ba7c",
+    "url": "/static/js/runtime-main.3fd8b123.js"
+  },
+  {
+    "revision": "3a886a5b90c687d0904b884b639157cc",
+    "url": "/static/media/overview.3a886a5b.png"
+  }
+]);
